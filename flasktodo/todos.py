074b732ec32481686a913abd23426d6461eb6c6d
LeeEnck <eaglesfootball33@gmail.com>
2020-03-31 12:56:53 -0400

    Added some css to the feature, and made a filter class in the index, also made the routing in todos.py

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
 
 from . import db
-
+import datetime
 
 bp = Blueprint("todos", __name__)
 
-@bp.route("/")
+@bp.route("/", methods=('GET', 'POST'))
 def index():
     """View for home page which shows list of to-do items."""
 
@@ -16,3 +17,27 @@ def index():
 
     return render_template("index.html", todos=todos)
 
+@bp.route("/completed", methods=('GET', 'POST'))
+def completed():
+
+
+    cur = db.get_db().cursor()
+
+    cur.execute('SELECT * FROM todos WHERE completed = True')
+    todos = cur.fetchall()
+    cur.close()
+
+    return render_template("index.html", todos=todos)
+
+
+@bp.route("uncompleted", methods=('GET'))
+def uncompleted():
+
+
+    cur = db.get_db().cursor()
+
+    cur.execute('SELECT * FROM todos WHERE completed = FALSE')
+    todos = cur.fetchall()
+    cur.close()
+
+    return render_template("index.html", todos=todos)
